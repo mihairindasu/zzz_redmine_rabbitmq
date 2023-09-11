8832dacd15a0119f521e88d5e6418809475a89cb
Mihai Rindasu <claudiu.rindasu@leviatan.ro>
2023-09-11 17:35:15 +0300

    + Added patch to issues controller to send the sprint_id in each issue

--- init.rb
+++ init.rb
@@ -8,6 +8,7 @@ require_dependency 'initializers/setup_rabbitmq'
 require_dependency 'redmine_rabbitmq/hooks'
 require_dependency 'project_patch'
 require_dependency 'sprint_patch'
+require_dependency 'issues_controller_after_action_patch'
 
 if defined?(Sprint)
   Sprint.send(:include, RedmineRabbitmq::Patches::SprintPatch) unless Sprint.included_modules.include? RedmineRabbitmq::Patches::SprintPatch
@@ -18,7 +19,7 @@ end
 Redmine::Plugin.register :redmine_rabbitmq do
   name 'Redmine Rabbitmq plugin'
   author 'eConfaire ID'
-  description 'This is a plugin for redmine that helps you send messages to RabbitMQ when certain events occur. The events are: issue creation, issue update, user login, project creation, project update, sprint creation, sprint update. It also has a rake task that can be used to send failed messages to RabbitMQ. And a patch to sprint model to get all sprints.'
+  description 'This is a plugin for redmine that helps you send messages to RabbitMQ when certain events occur. The events are: issue creation, issue update, user login, project creation, project update, sprint creation, sprint update. It also has a rake task that can be used to send failed messages to RabbitMQ. And a patch to sprint model to get all sprints. For sprints it needs the Scrum plugin installed.'
   version '0.0.1'
   url 'https://econfaire.ro/'
   author_url 'https://econfaire.ro/'
---
+++ patches/issues_controller_after_action_patch.rb
@@ -0,0 +1,43 @@
+
+require_dependency 'issues_controller'
+
+module RedmineRabbitmq
+  module Patches
+    module IssuesControllerAfterActionPatch
+      def self.included(base)
+        base.send(:include, InstanceMethods)
+        
+        base.class_eval do
+          unloadable
+          after_action :modify_response_include_sprint_id, only: [:index, :show]
+        end
+      end
+
+      module InstanceMethods
+        def modify_response_include_sprint_id
+          Rails.logger.info "IssuesController#modify_response_include_sprint_id"
+          
+          begin
+            modified_data = JSON.parse(response.body)
+
+            if action_name == "show" && @issue.respond_to?(:sprint_id)
+              modified_data["issue"]["sprint_id"] = @issue.sprint_id
+            elsif action_name == "index" && @issues.first&.respond_to?(:sprint_id)
+              modified_data["issues"].each_with_index do |issue_data, idx|
+                issue_data["sprint_id"] = @issues[idx].sprint_id
+              end
+            end
+
+            # Modify the response body directly without using render
+            response.body = modified_data.to_json
+          rescue JSON::ParserError => e
+            Rails.logger.error "Error parsing response JSON in IssuesController#modify_response_include_sprint_id: #{e.message}"
+          end
+        end
+      end
+    end
+  end
+end
+
+# Apply the patch to the IssuesController
+IssuesController.send(:include, RedmineRabbitmq::Patches::IssuesControllerAfterActionPatch)
